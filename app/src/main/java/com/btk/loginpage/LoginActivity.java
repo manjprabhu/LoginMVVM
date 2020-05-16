8783b956915cfbb5c4a12df151894927d90d9031
MPB <mp4learning@gmail.com>
2020-05-16 20:16:51 +0530

    Login Sample : Working - 1

--- app/src/main/java/com/btk/loginpage/LoginActivity.java
+++ app/src/main/java/com/btk/loginpage/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.btk.loginpage;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
-import android.os.Bundle;
-
 import com.btk.loginpage.databinding.ActivityMainBinding;
+
+import io.reactivex.MaybeObserver;
+import io.reactivex.disposables.Disposable;
 
 public class LoginActivity extends AppCompatActivity implements ButtonClickCallback {
 
@@ -18,13 +22,62 @@ public class LoginActivity extends AppCompatActivity implements ButtonClickCallb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        mainBinding.setCallback(this);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mViewmodel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        mainBinding.setCallback(this);
     }
 
     @Override
     public void onLoginClick() {
 
+       /* if(1<= mainBinding.etUsername.getText().toString().length()) {
+            mViewmodel.loginClick(mainBinding.etUsername.getText().toString(), mainBinding.etPassword.getText().toString()).subscribeOn(Schedulers.io())
+                    .subscribe(new DisposableMaybeObserver<Boolean>() {
+                        @Override
+                        public void onSuccess(Boolean o) {
+                            Log.v(TAG, "onSuccess :" + o);
+                            runOnUiThread(() -> Toast.makeText(LoginActivity.this, "Successful Login", Toast.LENGTH_SHORT).show());
+                            WelcomeActivity.start(LoginActivity.this, mainBinding.etUsername.getText().toString(),true);
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Log.v(TAG, "onError :");
+                            runOnUiThread(() -> Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show());
+                        }
+
+                        @Override
+                        public void onComplete() {
+                            Log.v(TAG, "onComplete :");
+                        }
+                    });
+        }*/
+
+        if (1 <= mainBinding.etUsername.getText().toString().length()) {
+            mViewmodel.loginClick(mainBinding.etUsername.getText().toString(), mainBinding.etPassword.getText().toString())
+                    .subscribe(new MaybeObserver<Boolean>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+                    Log.v(TAG, "onSubscribe");
+                 }
+
+                @Override
+                public void onSuccess(Boolean aBoolean) {
+                    Log.v(TAG, "Onsuccess:" + aBoolean);
+                    runOnUiThread(() -> Toast.makeText(LoginActivity.this, "Successful Login", Toast.LENGTH_SHORT).show());
+                    WelcomeActivity.start(LoginActivity.this, mainBinding.etUsername.getText().toString(),true);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    Log.v(TAG, "onError:");
+                }
+
+                @Override
+                public void onComplete() {
+                    Log.v(TAG, "onComplete:");
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/btk/loginpage/LoginViewModel.java
+++ app/src/main/java/com/btk/loginpage/LoginViewModel.java
@@ -2,6 +2,24 @@ package com.btk.loginpage;
 
 import androidx.lifecycle.ViewModel;
 
+import io.reactivex.Maybe;
+import io.reactivex.MaybeEmitter;
+
 public class LoginViewModel extends ViewModel {
 
+    public Maybe<Boolean> loginClick(final String userName, final String password) {
+        return Maybe.create(emitter -> validateCredentials(emitter, userName, password));
+    }
+
+    private void validateCredentials(MaybeEmitter<Boolean> emitter, String username, String password) {
+        if(validateUserName(username)) {
+            emitter.onSuccess(true);
+        } else {
+            emitter.onError(new Throwable("Autentication failed"));
+        }
+    }
+
+    private boolean validateUserName(String str) {
+        return !str.matches("[0-9]+");
+    }
 }
